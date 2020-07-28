516a7a474d8214ee4ccdc2fe375206bc828f77b3
Aditya Kumar <adikmr567@gmail.com>
2020-07-28 16:41:14 +0530

    Widget for transaction list added with external package

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,33 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
+import './transaction_widget.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        amount: 200, id: 't1', date: DateTime.now(), title: 'HDMI to VGA'),
+    Transaction(amount: 300, id: 't2', date: DateTime.now(), title: 'BILL')
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Expense Tracker',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Expense Tracker"),
-        ),
-        body: Center(
-          child: Text(
-            'Hello WOrld',
-            style: TextStyle(fontSize: 20),
+          appBar: AppBar(
+            title: Text("Expense Tracker"),
           ),
-        ),
-      ),
+          body: Column(
+            children: <Widget>[
+              TransactionWidget(transactions[0]),
+              TransactionWidget(transactions[1]),
+            ],
+          )),
     );
   }
 }
---
+++ lib/transaction_widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'transaction.dart';
+
+class TransactionWidget extends StatelessWidget {
+  final Transaction transaction;
+
+  TransactionWidget(this.transaction);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        child: Row(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  border: Border.all(width: 5, color: Colors.amber[200]),
+                  shape: BoxShape.circle,
+                  color: Colors.amber[200]),
+              child: Text(
+                "₹${transaction.amount}",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  '${transaction.title}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                Text("${transaction.date}"),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
