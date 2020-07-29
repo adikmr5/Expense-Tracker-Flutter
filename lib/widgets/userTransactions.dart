dc44baf02a898f6903469721d7da7ece6f8ea856
Aditya Kumar <adikmr567@gmail.com>
2020-07-29 21:57:18 +0530

    Removed mapping here

--- lib/widgets/userTransactions.dart
+++ lib/widgets/userTransactions.dart
@@ -9,11 +9,6 @@ class UserTransactions extends StatefulWidget {
 }
 
 class _UserTransactionsState extends State<UserTransactions> {
-  final List<Transaction> transactions = [
-    Transaction(
-        amount: 200, id: 't1', date: DateTime.now(), title: 'HDMI to VGA'),
-    Transaction(amount: 300, id: 't2', date: DateTime.now(), title: 'BILL'),
-  ];
   void addTx(String title, int amount) {
     Transaction newTx = new Transaction(
         amount: amount,
@@ -25,12 +20,17 @@ class _UserTransactionsState extends State<UserTransactions> {
     });
   }
 
+  final List<Transaction> transactions = [
+    Transaction(
+        amount: 200, id: 't1', date: DateTime.now(), title: 'HDMI to VGA'),
+    Transaction(amount: 300, id: 't2', date: DateTime.now(), title: 'BILL'),
+  ];
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         NewTransaction(addTx),
-        ...transactions.map((e) => TransactionWidget(e)).toList()
+        TransactionWidget(transactions),
       ],
     );
   }
