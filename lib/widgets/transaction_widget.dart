1bde35de1b8b7312733234299c0c9e293192bf15
Aditya Kumar <adikmr567@gmail.com>
2020-07-29 21:58:06 +0530

    Added ListView Builder and mapped

--- lib/widgets/transaction_widget.dart
+++ lib/widgets/transaction_widget.dart
@@ -3,44 +3,52 @@ import '../models/transaction.dart';
 import 'package:intl/intl.dart';
 
 class TransactionWidget extends StatelessWidget {
-  final Transaction transaction;
+  final List<Transaction> transactions;
 
-  TransactionWidget(this.transaction);
+  TransactionWidget(this.transactions);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        child: Row(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(20),
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                  border: Border.all(width: 5, color: Colors.amber[200]),
-                  shape: BoxShape.circle,
-                  color: Colors.amber[200]),
-              child: Text(
-                "₹${transaction.amount}",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  '${transaction.title}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
+    return Container(
+        height: 300,
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return Card(
+              child: Container(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.all(20),
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          border:
+                              Border.all(width: 5, color: Colors.amber[200]),
+                          shape: BoxShape.circle,
+                          color: Colors.amber[200]),
+                      child: Text(
+                        "₹${transactions[index].amount}",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          '${transactions[index].title}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                        Text(DateFormat().format(transactions[index].date)),
+                      ],
+                    )
+                  ],
                 ),
-                Text(DateFormat().format(transaction.date)),
-              ],
-            )
-          ],
-        ),
-      ),
-    );
+              ),
+            );
+          },
+          itemCount: transactions.length,
+        ));
   }
 }
